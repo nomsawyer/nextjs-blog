889204eef9bc25093f3069e33798d6abfbf9490d
tokio <tokio0813@gmail.com>
2021-01-10 11:23:36 +0900

    Replace JavaScript with TypeScript

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -9,7 +9,7 @@ const postsDirectory = path.join(process.cwd(), 'posts')
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
 
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string })
     }
   })
   // Sort posts by date
@@ -38,46 +38,32 @@ export function getSortedPostsData() {
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory)
-
-  // 以下のような配列を返します:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return fileNames.map((fileName) => {
+  return fileNames.map(fileName => {
     return {
       params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
+        id: fileName.replace(/\.md$/, '')
+      }
     }
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
-  // 投稿のメタデータ部分を解析するために gray-matter を使う
+  // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents)
 
-  // マークダウンを HTML 文字列に変換するために remark を使う
+  // Use remark to convert markdown into HTML string
   const processedContent = await remark()
     .use(html)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
 
-  // データを id および contentHtml と組み合わせる
+  // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string })
   }
 }
